a850615b7a6ce2191b4e6d2ab5e4219339d0abde
Timothy Castiglia <castit@rpi.edu>
2020-01-06 14:38:20 -0500

    UDP sockets in Raft

--- raft/raft.py
+++ raft/raft.py
@@ -10,24 +10,65 @@ import sys
 import os
 import grpc
 import threading
+import multiprocessing
 from concurrent import futures
 import math
 import time
 import random
+import socket
 
-import raft_pb2
-import raft_pb2_grpc
+class message():
+    __init__(self, func, obj):
+        self.func = func
+        self.obj = obj
+
+class LogEntry():
+    __init__(self, term, data, proposer):
+        self.term = term;
+        self.data = data;
+        self.proposer = proposer;
+
+class Entry():
+    __init__(self, entry):
+        self.entry = entry
+
+class Proposal():
+    __init__(self, entry, proposer):
+        self.entry = entry;
+        self.proposer = proposer;
+
+class Entries():
+    __init__(self, term, leaderId, prevLogIndex,
+            prevLogTerm, entries, leaderCommit):
+        self.term = term;
+        self.leaderId = leaderId;
+        self.prevLogIndex = prevLogIndex;
+        self.prevLogTerm = prevLogTerm;
+        self.entries = entries;
+        self.leaderCommit = leaderCommit;
+
+class Ack():
+    __init__(self, term, success):
+        self.term = term;
+        self.success = success;
+
+class VoteRequest():
+    __init__(self, term, candidateId, lastLogIndex, lastLogTerm)
+        self.term = term;
+        self.candidateId = candidateId;
+        self.lastLogIndex = lastLogIndex;
+        self.lastLogTerm = lastLogTerm;
 
 DEBUG = True
 # Stable storage of all servers as defined in the Raft paper.
 currentTerm = 0;
-log = [raft_pb2.LogEntry(data = "NULL", term = 0)];
+log = [LogEntry(data = "NULL", term = 0)];
 votedFor = "";
 
 # Volatile state of all servers
 commitIndex = 0;
 lastApplied = 0;
-leaderId = "";
+leaderId = None;
 
 # Volatile state of leaders
 nextIndex = {}
@@ -40,14 +81,19 @@ instance_file = open("instances.txt", 'r')
 members = []
 lines = instance_file.readlines()
 for line in lines:
-    members.append(line[0:-1]+":8100") 
+    members.append((int(line[0:-1]),8100)) 
 instance_file.close()
 
 # Read in own host name
 my_port = 8100 
 host_file = open("host_name.txt", 'r')
-this_id = host_file.readlines()[0][0:-1]+":8100"
+this_id = (int(host_file.readlines()[0][0:-1]),8100)
 host_file.close()
+
+# Create socket for listening and sending
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP	
+sock.bind(this_id)
 
 os.system("touch /home/ubuntu/"+this_id+".txt")
 
@@ -67,8 +113,11 @@ def print_log():
         
         print("{}\t{}\t{}".format(i,log[i].term,log[i].data))
 
-def ack(success):
-    return raft_pb2.Ack(term = currentTerm, success = success)
+def ack(success, server):
+    global sock
+    new_message = Message("ACK", Ack(term = currentTerm, success = success))
+    message_string = pickle.dumps(new_message)
+    sock.sendto(message_string, server)
 
 def term_equal(log_index, term):
     if log_index < 0:
@@ -78,144 +127,131 @@ def term_equal(log_index, term):
     return log[log_index].term == term
 
 def propose(entry, server): 
-    global commitIndex
-    if server == "":
+    global sock
+    if server is None:
         return
-    with grpc.insecure_channel(server) as channel:
-        try:
-            stub = raft_pb2_grpc.RaftStub(channel)
-            debug_print("Sending Proposal to {}".format(server))
-            response = stub.ReceivePropose(raft_pb2.Proposal(entry = entry, 
-                                                   proposer = this_id), timeout=5)
-        except grpc.RpcError as e:
-            debug_print(e)
-            debug_print("couldn't connect to {}".format(server))
+    debug_print("Sending Proposal to {}".format(server))
+    new_message = Message("ReceivePropose", Proposal(entry = entry, 
+                                           proposer = this_id))
+    message_string = pickle.dumps(new_message)
+    sock.sendto(message_string, server)
 
-class Raft(raft_pb2_grpc.RaftServicer):
+def ReceivePropose(request):
+    global log
+    log.append(request.entry)
 
-    def ReceivePropose(self,request,context):
-        global log
-        log.append(request.entry)
-        return ack(True)
+def AppendEntries(request):
+    global log, commitIndex, currentTerm, leaderId
+    global election_timer, first, run, propose_time
 
-    def AppendEntries(self,request,context):
-        global log, commitIndex, currentTerm, leaderId
-        global election_timer, first, run, propose_time
+    debug_print("Received AppendEntries from {}".format(request.leaderId))
+    if request.term < currentTerm:
+        ack(False, request.leaderId)
+        return
+    if not term_equal(request.prevLogIndex, request.prevLogTerm):
+        ack(False, request.leaderId)
+        return
+    leaderId = request.leaderId
 
-
-        debug_print("Received AppendEntries from {}".format(request.leaderId))
-        if request.term < currentTerm:
-            return ack(False)
-        if not term_equal(request.prevLogIndex, request.prevLogTerm):
-            return ack(False)
-        leaderId = request.leaderId
-
-        if first:
-            first = False
-            propose_time = True
-            run = threading.Timer(60*3, stop_running)
-            run.start()
-
-        if leaderId != this_id:
-            pass
-            #election_timer.cancel()
-            #randTime = random.randint(250,500)
-            #election_timer = threading.Timer(randTime/100.0, election_timeout) 
-            #election_timer.start()
-        if request.term > currentTerm:
-            global current_state
-            current_state = "follower"
-            currentTerm = request.term
-        
-        log = log[:request.prevLogIndex+1]
-
-        for entry in request.entries:
-            log.append(entry)
-            print("appended entry: {} to log in index {}".format(entry.data, len(log)-1))
-
-        oldCommitIndex = commitIndex
-        commitIndex = min(request.leaderCommit, len(log) -1)
-        if commitIndex > oldCommitIndex:
-            debug_print("committing to {}".format(commitIndex))
-
-        return ack(True)
-
-
-    def RequestVote(self,request,context):
-        global currentTerm
-        if request.term < currentTerm:
-            return ack(False)
-        if request.term > currentTerm:
-            global current_state
-            current_state = "follower"
-            currentTerm = request.term
-
-        #Haven't yet committed vote
-        if (votedFor == "" or votedFor == request.candidateId):
-            if (request.lastLogIndex >= len(log)-1 and request.lastLogTerm >= log[-1].term) or request.lastLogTerm > log[-1].term:
-                debug_print("Voted for {}".format(request.candidateId))
-                return ack(True)
-        return ack(False)
-
-    def Notified(self,request,context):
-        global start_times, propose_time
-        t = start_times[request.entry.data]
-        elapsed_time = time.time() - t
-        f=open("/home/ubuntu/"+this_id+".txt", "a+")
-        f.write(str(elapsed_time)+"\n")
-        f.close()
+    if first:
+        first = False
         propose_time = True
-        return ack(True)
+        run = threading.Timer(60*3, stop_running)
+        run.start()
+
+    if leaderId != this_id:
+        pass
+        #election_timer.cancel()
+        #randTime = random.randint(250,500)
+        #election_timer = threading.Timer(randTime/100.0, election_timeout) 
+        #election_timer.start()
+    if request.term > currentTerm:
+        global current_state
+        current_state = "follower"
+        currentTerm = request.term
+    
+    log = log[:request.prevLogIndex+1]
+
+    for entry in request.entries:
+        log.append(entry)
+        print("appended entry: {} to log in index {}".format(entry.data, len(log)-1))
+
+    oldCommitIndex = commitIndex
+    commitIndex = min(request.leaderCommit, len(log) -1)
+    if commitIndex > oldCommitIndex:
+        debug_print("committing to {}".format(commitIndex))
+
+    ack(True, request.leaderId)
+
+
+#def RequestVote(request):
+#    global currentTerm
+#    if request.term < currentTerm:
+#        return ack(False)
+#    if request.term > currentTerm:
+#        global current_state
+#        current_state = "follower"
+#        currentTerm = request.term
+#
+#    #Haven't yet committed vote
+#    if (votedFor == "" or votedFor == request.candidateId):
+#        if (request.lastLogIndex >= len(log)-1 and request.lastLogTerm >= log[-1].term) or request.lastLogTerm > log[-1].term:
+#            debug_print("Voted for {}".format(request.candidateId))
+#            return ack(True)
+#    return ack(False)
+
+def Notified(request):
+    global start_times, propose_time
+    t = start_times[request.entry.data]
+    elapsed_time = time.time() - t
+    f=open("/home/ubuntu/"+this_id+".txt", "a+")
+    f.write(str(elapsed_time)+"\n")
+    f.close()
+    propose_time = True
 
 def send_append_entries(server,heartbeat):
     global nextIndex, matchIndex, commitIndex, currentTerm, log
-    with grpc.insecure_channel(server) as channel:
-        try:
-            stub = raft_pb2_grpc.RaftStub(channel)
-            prev_index = nextIndex[server]-1
-            prev_term = 0
-            if len(log) > prev_index and prev_index >= 0:
-                prev_term = log[prev_index].term
-            if heartbeat:
-                entries = []
-            else:
-                entries = log[prev_index+1:]
-            debug_print("Sending AppendEntries to {} with prev_index {}".format(server,prev_index))
-            response = stub.AppendEntries(raft_pb2.Entries(term = currentTerm, leaderId = this_id, prevLogIndex = prev_index, prevLogTerm = prev_term, entries=entries,leaderCommit = commitIndex), timeout=5)
-            if response.term > currentTerm:
-                global current_state
-                currentTerm = response.term
-                current_state = "follower"
-                return False
-            if not response.success:
-                nextIndex[server] -=1
-                send_append_entries(server,heartbeat)
-            if response.success and not heartbeat:
-                nextIndex[server] = len(log)
-                matchIndex[server] = len(log)-1
-        except grpc.RpcError as e:
-            debug_print(e)
-            debug_print("couldn't connect to {}".format(server))
-    return matchIndex[server]
+    global sock
+    prev_index = nextIndex[server]-1
+    prev_term = 0
+    if len(log) > prev_index and prev_index >= 0:
+        prev_term = log[prev_index].term
+    if heartbeat:
+        entries = []
+    else:
+        entries = log[prev_index+1:]
+    debug_print("Sending AppendEntries to {} with prev_index {}".format(server,prev_index))
+    new_message = Message("AppendEntries", Entries(term = currentTerm, leaderId = this_id, prevLogIndex = prev_index, prevLogTerm = prev_term, entries=entries,leaderCommit = commitIndex))
+    message_string = pickle.dumps(new_message)
+    sock.sendto(message_string, server)
 
+def AppendEntriesResp(response):
+    if response.term > currentTerm:
+        global current_state
+        currentTerm = response.term
+        current_state = "follower"
+        return False
+    if not response.success:
+        nextIndex[server] -=1
+        send_append_entries(server,heartbeat)
+    if response.success and not heartbeat:
+        nextIndex[server] = len(log)
+        matchIndex[server] = len(log)-1
 
 def notify(server, entry):
-    if server == "":
+    global sock
+    if server is None:
         return
-    with grpc.insecure_channel(server) as channel:
-        try:
-            stub = raft_pb2_grpc.RaftStub(channel)
-            debug_print("Notifying {}".format(server))
-            response = stub.Notified(raft_pb2.Entry(entry = entry), timeout=5)
-        except grpc.RpcError as e:
-            debug_print(e)
-            debug_print("couldn't connect to {}".format(server))
-
+    debug_print("Notifying {}".format(server))
+    new_message = Message("Notified", Entry(entry = entry))
+    message_string = pickle.dumps(new_message)
+    sock.sendto(message_string, server)
 
 def update_everyone(heartbeat):
     global commitIndex
     for server in members:
         send_append_entries(server,heartbeat)
+
     new_commit_index = commitIndex
     for i in range(commitIndex+1,len(log)):
         greater_index = [index for index in matchIndex.values() if index >= i]
@@ -240,45 +276,65 @@ def become_leader():
 
     update_everyone(True)
 
-def hold_election():
-    global currentTerm,matchIndex,current_state
-    currentTerm += 1
-    votedFor = this_id
-    vote_count = 1
-    for server in members:
-        if server != this_id:
-            with grpc.insecure_channel(server) as channel:
-                stub = raft_pb2_grpc.RaftStub(channel)
-                try:
-                    response = stub.RequestVote(raft_pb2.VoteRequest(term = currentTerm, candidateId = this_id, lastLogIndex = len(log)-1, lastLogTerm = log[-1].term), timeout=5)
-                    if response.success:
-                        vote_count +=1
-                        debug_print("received vote from {}".format(server))
-                    if response.term > currentTerm:
-                        current_state = "follower"
-                        currentTerm = response.term
-                except grpc.RpcError as e:
-                    debug_print("couldn't connect to {}".format(server))
-    if vote_count >= len(members)/2:
-        current_state = "leader"
-        become_leader()
-    else:
-        debug_print("lost election")
-        current_state = "follower"
-        global election_timer
-        randTime = random.randint(250,500)
-        election_timer = threading.Timer(randTime/100.0, election_timeout) 
-        election_timer.start()
+#def hold_election():
+#    global currentTerm,matchIndex,current_state
+#    currentTerm += 1
+#    votedFor = this_id
+#    vote_count = 1
+#    for server in members:
+#        if server != this_id:
+#            with grpc.insecure_channel(server) as channel:
+#                stub = raft_pb2_grpc.RaftStub(channel)
+#                try:
+#                    response = stub.RequestVote(raft_pb2.VoteRequest(term = currentTerm, candidateId = this_id, lastLogIndex = len(log)-1, lastLogTerm = log[-1].term), timeout=5)
+#                    if response.success:
+#                        vote_count +=1
+#                        debug_print("received vote from {}".format(server))
+#                    if response.term > currentTerm:
+#                        current_state = "follower"
+#                        currentTerm = response.term
+#                except grpc.RpcError as e:
+#                    debug_print("couldn't connect to {}".format(server))
+#    if vote_count >= len(members)/2:
+#        current_state = "leader"
+#        become_leader()
+#    else:
+#        debug_print("lost election")
+#        current_state = "follower"
+#        global election_timer
+#        randTime = random.randint(250,500)
+#        election_timer = threading.Timer(randTime/100.0, election_timeout) 
+#        election_timer.start()
 
-
-
+def receive_message(data, sock):
+    message = pickle.loads(message_string)
+    if message.func == "ReceivePropose":
+        ReceivePropose(message.obj)
+    elif message.func == "AppendEntries":
+        AppendEntries(message.obj)
+    elif message.func == "ACK":
+        AppendEntriesResp(message.obj)
+    #elif message.func == "RequestVote":
+    #    RequestVote(message.obj)
+    elif message.func == "Notified":
+        Notified(message.obj)
 
 def start_grpc_server():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=200))
-    raft_pb2_grpc.add_RaftServicer_to_server(Raft(), server)
-    server.add_insecure_port('[::]:{}'.format(my_port))
-    server.start()
-    server.wait_for_termination()
+    global sock
+    while running:
+            data, addr = sock.recvfrom(1048576) # buffer size is 2^20 bytes
+            # To have a way to safely quit, quit message breaks loop
+            try:
+                if data.decode('UTF-8') == "quit":
+                    break
+            except UnicodeDecodeError:
+                pass
+
+            # Make thread for receive handling
+            receive_thread = multiprocessing.Process(
+                    target=receive_message,
+                    args=(data, sock, proc_ids))
+            receive_thread.start()
 
 
 """
@@ -340,7 +396,7 @@ def main(args):
             update_everyone(False)
         if propose_time and args[1] == "propose":
             propose_time = False
-            entry = raft_pb2.LogEntry(data = str(counter), 
+            entry = LogEntry(data = str(counter), 
                                           term = currentTerm,
                                           proposer = this_id)
             start_times[str(counter)] = time.time()
