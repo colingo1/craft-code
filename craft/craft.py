65ec1ce173d8ac54628ffd8b2eac74892b221d30
Timothy Castiglia <castit@rpi.edu>
2020-04-03 17:19:39 -0400

    print inputs are ambiguous

--- craft/craft.py
+++ craft/craft.py
@@ -125,7 +125,7 @@ first = True
 
 def debug_print(m):
     if DEBUG:
-        print(str(time.time())+": "+m)
+        print(str(time.time())+": "+str(m))
 
 debug_print(members)
 debug_print(this_id)
